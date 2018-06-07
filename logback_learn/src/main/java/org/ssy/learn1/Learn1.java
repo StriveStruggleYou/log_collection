3f0eb75fd41e643468d3ac09886b7a6a8aca520d
chendekai <chendekai@qipeng.com>
2018-06-07 15:46:16 +0800

    提交基础文件

---
+++ logback_learn/src/main/java/org/ssy/learn1/Learn1.java
@@ -0,0 +1,18 @@
+package org.ssy.learn1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by manager on 2018/6/7.
+ */
+public class Learn1 {
+
+
+
+  public static void main(String args[]) {
+    Logger logger = LoggerFactory.getLogger("org.ssy.learn1.Learn1");
+    logger.info("hello world");
+  }
+
+}
